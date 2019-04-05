986d22de161651b78ffa01a03cb903b48897d75f
Barbara Johnson <johnson.barbara@gmail.com>
2019-04-05 02:12:47 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 def oxford_comma(array)
   if array.length == 2 
     new_string = array.join(" and ")
-  elsif array.length == 3
+  elsif array.length % 2 && array.length >= 3
     last_item = array.pop 
     new_string = array.join(",")
     new_list= "#{new_string}, and  #{last_item}"
