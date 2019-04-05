e7d25071ef8ce006d73283b0bf7d5b2d95b4f6b3
Barbara Johnson <johnson.barbara@gmail.com>
2019-04-05 01:51:55 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,6 @@
 def oxford_comma(array)
-  new_string = array.join(" and ")
+  if array.length == 2 
+    new_string = array.join(" and ")
+  elsif array.length == 3
+    new_string = array.join(", ")
 end
